c708626a0d4e4c396dc9b9e7dc4604c1c94407f0
vivek7325 <68550321+vivek7325@users.noreply.github.com>
2023-07-30 18:48:36 +0530

    Add files via upload

---
+++ src/TodoInput.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+function TodoInput() {
+  return (
+    <div className="input-container">
+      <input type="text" className="input-box-code" />
+      <button className="add-btn">+</button>
+    </div>
+  );
+}
+export default TodoInput;
